9e143e7e2e561a73ed6a1af3e55bcfcf1f935ea6
Yuriy Belov <paganism@mail.ru>
2021-11-13 09:48:51 +0300

    rename list to readable

--- questions.py
+++ questions.py
@@ -2,10 +2,10 @@
 def read_file(filepath):
     questions = []
     with open(filepath, 'r', encoding='koi8-r') as file:
-        questions_list = file.read().split('\n\n')
+        readed_questions = file.read().split('\n\n')
         question = ''
         answer = ''
-        for item in questions_list:
+        for item in readed_questions:
             if item.startswith('Вопрос'):
                 question = item
             if item.startswith('Ответ'):
